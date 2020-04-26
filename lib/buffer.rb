be5a599abfdd9ddfbd2d8294a6552e351babe60b
adam <aabou9@gmail.com>
2020-04-26 04:40:09 +0100

    update Buffer#read_file

--- lib/buffer.rb
+++ lib/buffer.rb
@@ -5,11 +5,11 @@ class Buffer
     end
 
     def read_file
-        content_arr = ''
+        content_arr = []
         File.open(@file_path, 'r') { |line| content_arr = line.readlines.map(&:chomp) }
-        content_arr
+        @content_arr = content_arr
     end
-
+    attr_reader :content_s
 end
 
 code = Buffer.new('test.txt')
