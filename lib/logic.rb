b82eb422047bca670b2d2dea5014e04e3b9d3d41
adam <aabou9@gmail.com>
2020-04-27 04:17:46 +0100

    add trailing space

--- lib/logic.rb
+++ lib/logic.rb
@@ -1,10 +1,62 @@
+require 'strscan'
+
 module Logic
 
-  def err_printer
+  def check_indentation_declaration(content_arr)
+    content_arr.each_with_index do |val, i|
+      test = val.scan_until(/:/) 
+      val.reset
+      # p val.bol?
+      if test && val.scan(/\s+/) != '  '
+        err_printer(i+1, 1, nil)
+      end
+    end
+  end
 
+  def check_indentation_selector(content_arr)
+    content_arr.each_with_index do |val, i|
+      test = val.scan_until(/{/) 
+      val.reset
+      # p val.bol?
+      if test && val.scan(/\s+/) != ''
+        err_printer(i+1, 1, nil)
+      end
+    end
+  end
 
+  def trailing_space(content_arr)
+    content_arr.each_with_index do |val, i|
+      test = val.string.split('')[-1]
+      if test == ' '
+        err_printer(i+1, 6, nil)
+      end
+    end
+  end
+
+  def err_printer(line, type, char)
+
+    case type
+    when 1
+      puts "Error: line #{line}, Wrong Indentation, expected 2 spaces."
+    when 2
+      puts "Error: line #{line}, Spacing, expected single space after #{char}"
+    when 3
+      puts "Error: line #{line}, Spacing, expected single space before #{char}"
+    when 4
+      puts "Error: line #{line}, Line Format, Expected line break after #{char}"
+    when 5
+      puts "Error: line #{line}, Line Format, Expected one empty line after #{char}"
+    when 6
+      puts "Error: line #{line}, Spacing, Trailing space detected."
+    end
 
   end
 
 
 end
+
+include Logic
+
+trailing_space([StringScanner.new('background-color: red;'), StringScanner.new('    background-color: red;'), StringScanner.new('p {'), StringScanner.new('p { ')])
+check_indentation_selector([StringScanner.new('background-color: red;'), StringScanner.new('    background-color: red;'), StringScanner.new('p {'), StringScanner.new('p { ')])
+check_indentation_declaration([StringScanner.new('background-color: red;'), StringScanner.new('    background-color: red;'), StringScanner.new('p {'), StringScanner.new('p { ')])
